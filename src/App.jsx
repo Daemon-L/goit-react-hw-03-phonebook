e2d9b1a1192c9360212b21f221f44cd397eac8b8
Daemon-L <lit.dmitriy.ua@gmail.com>
2022-01-23 16:00:07 +0200

    Добавляет сохранение в local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -60,6 +60,22 @@ class App extends Component {
         }));
     };
 
+    componentDidMount() {
+        const contacts = localStorage.getItem('contacts');
+        const parsedContacts = JSON.parse(contacts);
+
+        if (parsedContacts) {
+            this.setState({ contacts: parsedContacts})
+        }
+    };
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.contacts !== prevState.contacts) {
+            console.log('new state.contacts');
+            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+        }
+
+    };
 
     render() { 
         const { filter } = this.state
